cdca36d6e37725d59a54ac1fbe48b9c193c7c3e4
Abhi Chadha <abhishekchadha235@gmail.com>
2020-11-02 21:37:39 +0530

    add storybook crate

--- src/components/CrateCard/index.tsx
+++ src/components/CrateCard/index.tsx
@@ -1,56 +1,84 @@
-import {
-  Badge,
-  Button,
-  Chip,
-  Container,
-  Fade,
-  Paper,
-  Typography,
-} from '@material-ui/core';
+import { Button, Chip, Paper, Typography } from '@material-ui/core';
+import Skeleton from '@material-ui/lab/Skeleton';
+import Web3 from 'web3';
+import { isNil } from 'lodash';
+import numeral from 'numeral';
 
 export interface CrateCardProps {
-  image: string;
-  id: number;
-  collection: {
-    name: string;
+  loading?: boolean;
+  token?: {
     image: string;
+    id: number;
+    backgroundColor: string;
+    collection: {
+      name: string;
+      image: string;
+    };
+    numberOfSales: number;
+    lastPrice: string;
+    maxPrice: string;
+    ranking: number;
+    hitRate: number;
   };
-  numberOfSales: number;
 }
 
 export default function CrateCard(props: CrateCardProps) {
+  if (props.loading) {
+    return <CrateCardSkeleton />;
+  }
+
   return (
-    <Paper className='max-w-lg h-auto flex p-1'>
+    <Paper className='max-w-lg h-48 flex p-1'>
       <div className='w-1/3'>
-        <img className='w-full rounded-sm' src={props.image} />
+        <div
+          className='h-full rounded-sm flex items-start justify-center'
+          style={{
+            backgroundColor: '#' + (props.token?.backgroundColor ?? 'white'),
+          }}
+        >
+          <img className='w-full' src={props.token?.image} />
+        </div>
       </div>
       <div className='flex-grow pl-2'>
         <div className='flex flex-row'>
           <img
             className='rounded-full w-10 h-10'
-            src={props.collection.image}
+            src={props.token?.collection.image}
           />
           <div className='pl-1'>
-            <Typography className='text-xs'>{props.collection.name}</Typography>
-            <Typography className='text-base font-bold'>#{props.id}</Typography>
+            <Typography className='text-xs'>
+              {props.token?.collection.name}
+            </Typography>
+            <Typography className='text-base font-bold'>
+              #{props.token?.id}
+            </Typography>
           </div>
         </div>
         <div className='pt-1'>
-          <Typography className='text-xs'>Sales / Owners</Typography>
-          <Typography className='text-base font-bold'>2 / 2</Typography>
+          <Typography className='text-xs'>Sales</Typography>
+          <Typography className='text-base font-bold'>
+            {props.token?.numberOfSales}
+          </Typography>
         </div>
         <div>
           <Typography className='text-xs'>Last / Max</Typography>
-          <Typography className='text-base font-bold'>Ξ 2 / 2</Typography>
+          <Typography className='text-base font-bold'>
+            Ξ {formatPrice(props.token?.lastPrice)} /{' '}
+            {formatPrice(props.token?.maxPrice)}
+          </Typography>
         </div>
         <div className='flex flex-row'>
           <div className='flex-1'>
             <Typography className='text-xs'>Ranking</Typography>
-            <Typography className='text-base font-bold'>422,427,123</Typography>
+            <Typography className='text-base font-bold'>
+              {numeral(props.token?.ranking).format('0,0')}
+            </Typography>
           </div>
           <div className='flex-1'>
             <Typography className='text-xs'>Hit Rate</Typography>
-            <Typography className='text-base font-bold'>{'> '}0.34%</Typography>
+            <Typography className='text-base font-bold'>
+              {numeral(props.token?.hitRate).format('%')}
+            </Typography>
           </div>
         </div>
       </div>
@@ -72,3 +100,74 @@ export default function CrateCard(props: CrateCardProps) {
     </Paper>
   );
 }
+
+function CrateCardSkeleton() {
+  const TextBlock = () => (
+    <>
+      <Skeleton variant='rect' height={10} width={'80%'} />
+      <Skeleton variant='rect' height={17} width={'80%'} className='mt-1' />
+    </>
+  );
+  return (
+    <Paper className='max-w-lg h-48 flex p-1'>
+      <div className='w-1/3'>
+        <Skeleton variant='rect' height='100%' />
+      </div>
+      <div className='flex-grow pl-2'>
+        <div className='flex flex-row'>
+          <Skeleton
+            variant='circle'
+            width={'2.6rem'}
+            height={'2.6rem'}
+            className='rounded-full w-10 h-10'
+          />
+          <div className='pl-1 w-4/5'>
+            <Skeleton variant='rect' height={10} width={'74%'} />
+            <Skeleton
+              variant='rect'
+              height={17}
+              width={'74%'}
+              className='mt-1'
+            />
+          </div>
+        </div>
+        <div className='pt-1'>
+          <TextBlock />
+        </div>
+        <div>
+          <TextBlock />
+        </div>
+        <div className='pt-1'>
+          <TextBlock />
+        </div>
+        <div className='pt-1'>
+          <Skeleton variant='rect' height={17} width={'80%'} className='mt-1' />
+        </div>
+      </div>
+      <div className='flex-1 mr-1'>
+        <Skeleton variant='rect' height={25} width={80} className='ml-auto' />
+        <Skeleton
+          variant='rect'
+          height={25}
+          width={80}
+          className='mt-2 ml-auto'
+        />
+        <Skeleton
+          variant='rect'
+          height={30}
+          width={80}
+          style={{ borderRadius: 50 }}
+          className='mt-2 ml-auto'
+        />
+      </div>
+    </Paper>
+  );
+}
+
+const formatPrice = (p) => {
+  if (isNil(p)) {
+    return '-';
+  }
+  const fmtPrice = Web3.utils.fromWei(p, 'ether');
+  return fmtPrice.length < 7 ? fmtPrice : fmtPrice.slice(0, 7);
+};
--- src/stories/Crate.stories.tsx
+++ src/stories/Crate.stories.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 
 import CrateCard, { CrateCardProps } from '../components/CrateCard';
+import { fetchTokenData } from '../services/opensea';
+import mockToken from '../../mocks/token';
+import Web3 from 'web3';
 
 export default {
   title: 'CrateCard',
@@ -10,14 +13,29 @@ export default {
 
 const Template: Story = (args: CrateCardProps) => <CrateCard {...args} />;
 
+const token = mockToken[0];
+
 export const Basic = Template.bind({});
 const args: CrateCardProps = {
-  image: 'https://placehold.it/400x400',
-  id: 423432,
-  collection: {
-    name: 'CryptoKitties',
-    image: 'https://placehold.it/40x40',
+  token: {
+    image: token.image_preview_url,
+    backgroundColor: token.background_color,
+    id: token.id,
+    collection: {
+      name: token.collection.name,
+      image: token.collection.image_url,
+    },
+    numberOfSales: token.num_sales,
+    lastPrice: token.last_sale.total_price,
+    maxPrice: token.top_bid?.price,
+    ranking: 3214234,
+    hitRate: 0.54356,
   },
-  numberOfSales: 3,
 };
 Basic.args = args;
+
+export const Loading = Template.bind({});
+const loadingArgs: CrateCardProps = {
+  loading: true,
+};
+Loading.args = loadingArgs;
